6b157057ab30e65a35ddbdb57d27ad58d79747f5
Mohammed ARBAOUI <arb.med241@gmail.com>
2021-01-09 00:50:31 +0100

    add Custom app bar responsive
    add  a responsive app  bar

--- lib/screens/nav_screen.dart
+++ lib/screens/nav_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:web/data/data.dart';
 import 'package:web/screens/home_screen.dart';
+import 'package:web/widgets/custom_app_bar.dart';
 import 'package:web/widgets/widgets.dart';
 
 class NavScreen extends StatefulWidget {
@@ -23,15 +25,33 @@ class _NavScreenState extends State<NavScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
     return DefaultTabController(
         length: _icons.length,
         child: Scaffold(
+          appBar: Responsive.isDesktop(context)
+              ? PreferredSize(
+            preferredSize: Size(screenSize.width, 100.0),
+            child: CustomAppBar(
+              currentUser: currentUser,
+              icons: _icons,
+              selectedIndex: _selectedIndex,
+              onTap: (index) =>
+                  setState(() => _selectedIndex = index
+                  ),
+            ),
+
+          )
+              : null,
           body: _screens[_selectedIndex],
-          bottomNavigationBar: CustomTabBar(
+          bottomNavigationBar: !Responsive.isDesktop(context) ? CustomTabBar(
             icons: _icons,
             selectedIndex: _selectedIndex,
-            onTap: (index) => setState(() => _selectedIndex = index),
-          ),
-        ));
+            onTap: (index) =>
+                setState(() => _selectedIndex = index
+                ),
+          ) : const SizedBox.shrink(),
+        )
+    );
   }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,3 +1,7 @@
 export 'circle_button.dart';
 export 'post_container.dart';
 export 'custom_tab_bar.dart';
+export 'responsive.dart';
+export 'custom_app_bar.dart';
+export 'profile_avatar.dart';
+export 'user_card.dart';
