63e453d0213ab2c814f8fccbce8dc8a0b89b738b
Abderrahmane Arbaoui <a.arbaoui184@gmail.com>
2021-01-20 12:41:36 +0100

    update user model and add firebase dependencies

--- lib/models/UserModel.dart
+++ lib/models/UserModel.dart
@@ -1,6 +1,7 @@
 import 'dart:collection';
 import 'dart:convert';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:web/screens/login/signupForm.dart';
 
 UserModel userModelJson(String str) => UserModel.fromJson(jsonDecode(str));
@@ -9,52 +10,49 @@ String userModelToJson(UserModel data) => jsonEncode(data);
 
 class UserModel {
   String id;
-  String firstName;
-  String lastName;
+  String name;
   String username;
   String password;
+  String profileImageUrl;
   String email;
   HashMap<String, String> userIds;
 
   UserModel(
       {this.id,
-      this.firstName,
-      this.lastName,
+      this.name,
       this.username,
       this.password,
-      this.email,
+        this.profileImageUrl,
+        this.email,
       this.userIds});
 
   factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
       id: json['id'],
-      firstName: json['firstName'],
-      lastName: json['lastName'],
+      name: json['name'],
       username: json['username'],
       password: json['password'],
+      profileImageUrl: json['profileImageUrl'],
       email: json['email'],
       userIds: json['userIds']);
 
   Map<String, dynamic> toJson() => {
         "id": id,
-        "firstName": firstName,
-        "lastName": lastName,
+        "name": name,
         "username": username,
         "password": password,
+        "profileImageUrl": profileImageUrl,
         "email": email,
         "userIds": userIds,
-      };
+  };
 
-  String get userName => username;
-
-  String get firstname => firstName;
-
-  String get lastname => lastName;
-
-  String get pwd => password;
-
-  String get pseudoname => username;
-
-  String get mailadress => email;
-
-  String get userids => userids;
+  factory UserModel.fromDoc(DocumentSnapshot doc) {
+    return UserModel(
+      id: doc.documentID,
+      name: doc['name'],
+      username: doc['username'],
+      password: doc['password'],
+      profileImageUrl: doc['profileImageUrl'],
+      email: doc['email'] ?? '',
+    );
+  }
 }
