a4dbb28dd68eb0d753345154d20cd2844bf1c92b
Mohammed Arbaoui <arb.med241@gmail.com>
2021-02-03 08:35:33 +0100

    youtube screen fixed

--- lib/screens/homeYoutube/youtube_screen_mobile.dart
+++ lib/screens/homeYoutube/youtube_screen_mobile.dart
@@ -1,24 +1,93 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:web/config/palette.dart';
 import 'package:web/data/data.dart';
 import 'package:web/models/models.dart';
+import 'package:web/screens/login/loginForm.dart';
 import 'package:web/screens/login/logscreen.dart';
+import 'package:web/services/api_service.dart';
+import 'package:web/services/database_service.dart';
+import 'package:web/utilities/keys.dart';
 import 'package:web/widgets/widgets.dart';
+import 'package:http/http.dart' as http;
 
 import 'home_screen.dart';
 
-class HomeScreenYoutubeMobile extends StatelessWidget {
-  final List<String> channelsID = ["UCiRDO4sVx9dsyMm9F7eWMvw", "UC8RszAOoVLLFgkkZoqwr5ug", "UCb3c6rB0Ru1i9EUcc-a5ZJw"];
+class HomeScreenYoutubeMobile extends StatefulWidget {
   final TrackingScrollController scrollController;
 
    HomeScreenYoutubeMobile({Key key, @required this.scrollController})
       : super(key: key);
 
   @override
+  _HomeScreenYoutubeMobileState createState() => _HomeScreenYoutubeMobileState();
+}
+
+class _HomeScreenYoutubeMobileState extends State<HomeScreenYoutubeMobile> {
+  final List<String> channelsID = ["UC2t5bjwHdUX4vM2g8TRDq5g","UCiRDO4sVx9dsyMm9F7eWMvw", "UCb3c6rB0Ru1i9EUcc-a5ZJw" ];
+
+  String userYoutubeChannelName="wa3errr";
+  String channel_id;
+  bool _isLoading ;
+
+
+  List<dynamic> _list;
+  @override
+  void initState() {
+    super.initState();
+    _isLoading = false;
+
+    _getListuserChannelYoutube();
+  }
+
+
+  _getListuserChannelYoutube() async{
+    print("_getlistuserchannel");
+   List<String> list =  await DatabaseService().listofYoutubefromFollowedUsers(box.read("currentUserId")) ;
+   print(list);
+   print(box.read("currentUserId"));
+   setState(() {
+     if(list.isNotEmpty){
+       _list= list;
+       box.write("_list",list);
+       box.write("_listSize", list.length);
+       print("listsize");
+       print(box.read("_listSize"));
+
+        for(int i = 0 ; i < box.read("_listSize"); i++ ){
+          String channelID = box.read("_list")[i];
+          print(box.read("_list")[i]);
+          _initChannel(i, channelID);
+     }
+
+     }
+
+   });
+  }
+
+  _initChannel(int index, String userYoutubeChannelName) async {
+    print("_initChannel");
+
+  String _channelId = await APIService.instance
+        .getlistchannelIDfromUserFollowedUsers(userYoutubeChannelName);
+  setState(() {
+      if(_channelId.isNotEmpty){
+        channel_id= _channelId;
+        box.write("channel_id$index",_channelId);
+        print(box.read("channel_id$index"));
+         _isLoading = false;
+      }else
+       _isLoading = true;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return CustomScrollView(
-      controller: scrollController,
+      controller: widget.scrollController,
       slivers: [
         SliverAppBar(
           brightness: Brightness.light,
@@ -74,11 +143,12 @@ class HomeScreenYoutubeMobile extends StatelessWidget {
         ),
         SliverList(
             delegate: SliverChildBuilderDelegate(
-                  (context, index) {
-                String channelID = channelsID[index];
-                return HomeScreenYTB( channelID: channelID);
+                  (context, index)
+              {
+
+                return !_isLoading ? HomeScreenYTB( channelID: box.read("channel_id$index")) : LinearProgressIndicator();
               },
-              childCount: channelsID.length,
+              childCount: box.read("_listSize"),
             )
         ),
       ],
