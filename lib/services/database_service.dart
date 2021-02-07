ae47643780ce15d36aaa5d59e77abf04965d7eb3
Mohammed Arbaoui <arb.med241@gmail.com>
2021-02-07 16:48:00 +0100

    fix  display  content on follow/unfollow user

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -126,31 +126,22 @@ class DatabaseService {
     var _usersRef = __firestore.collection('user');
     DocumentSnapshot listYoutubeChannel =
         await _usersRef.doc(currentUserId).get();
-    List<dynamic> data = listYoutubeChannel.data()["userFollowed"];
-    for(int i = 0; i<data.length; i++){
-      DocumentSnapshot youtubeChannel = await usersRef.doc(data[i]).get();
 
-      if (youtubeChannel.exists) {
-        String _data = youtubeChannel.data()["userYoutube"];
-        if (_data.isNotEmpty) {
-          result.add(_data.toString());
-        }
-      }
+     if(listYoutubeChannel.exists){
+       List<dynamic> data = listYoutubeChannel.data()["userFollowed"];
+       for(int i = 0; i<data.length; i++){
+         DocumentSnapshot youtubeChannel = await usersRef.doc(data[i]).get();
+         if (youtubeChannel.exists) {
+           String _data = youtubeChannel.data()["userYoutube"];
 
-    }
- /*   var varx = data.forEach((element) async {
-      DocumentSnapshot youtubeChannel = await usersRef.doc(element).get();
-      if (youtubeChannel.exists) {
-        String _data = youtubeChannel.data()["userYoutube"];
-        print("databseservice");
-        print(_data);
-        if (_data.isNotEmpty) {
-          result.add(_data.toString());
-        }
-      }
-    });
-    print("databseservice");
-    print(result);*/
+           if (_data.isNotEmpty) {
+             result.add(_data);
+           }
+         }
+
+       }
+     }
+
     print("databseservice");
     print(result);
     return result;
@@ -173,4 +164,30 @@ class DatabaseService {
 
     return userChannelName;
   }
+
+  numFollowedUser(String CurrentUserID) async{
+    List<String> followedUserList;
+    QuerySnapshot followingSnapshot =
+        await followingRef.doc(CurrentUserID).collection('userFollowing').get();
+
+    return followingSnapshot.docs.length;
+
+
+  }
+
+  static Future<List<UserModel>> getFollowingList(String userId) async {
+    QuerySnapshot usersSnapshot = await followingRef
+        .doc(userId)
+        .collection('userFollowing')
+        .get();
+
+    print(usersSnapshot);
+    List<UserModel> user = usersSnapshot.docs
+        .map((doc) => UserModel.fromDoc(doc))
+        .toList();
+    return user;
+  }
+
+
+
 }
