92eef167a7364c0ea212ad63413899fb36784eac
Mohammed Arbaoui <arb.med241@gmail.com>
2021-02-08 18:54:05 +0100

    update  nav screen order

--- lib/screens/nav_screen.dart
+++ lib/screens/nav_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:web/data/data.dart';
+import 'package:web/models/UserModel.dart';
 import 'package:web/screens/activity/activity_screen.dart';
+import 'package:web/screens/profil/profile_main_screen.dart';
 import 'package:web/screens/profil/profile_screen.dart';
 import 'package:web/screens/screens.dart';
+import 'package:web/services/database_service.dart';
 import 'package:web/widgets/custom_app_bar.dart';
 import 'package:web/widgets/widgets.dart';
 
@@ -18,25 +21,45 @@ class NavScreen extends StatefulWidget {
 }
 
 class _NavScreenState extends State<NavScreen> {
+  UserModel _user;
   final String currentUserId = box.read("currentUserId");
   final List<Widget> _screens = [
     HomeScreen(),
-    ProfileScreen(
+    HomeScreenTweets(),
+    HomeMainScreenYoutube(),
+    ActivityScreen(currentUserId: box.read("currentUserId")),
+
+    ProfileMainScreen(
       currentUserId: box.read("currentUserId"),
       userId: box.read("currentUserId"),
     ),
-    ActivityScreen(currentUserId: box.read("currentUserId")),
-    HomeScreenTweets(),
-    HomeMainScreenYoutube(),
+
   ];
   final List<IconData> _icons = const [
     Icons.home,
-    MdiIcons.accountCircleOutline,
-    MdiIcons.heart,
     MdiIcons.twitter,
     MdiIcons.youtube,
+  Icons.notifications,
+    MdiIcons.account,
+
+
   ];
   int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentUserInfo();
+  }
+
+  _getCurrentUserInfo()async{
+  UserModel user = await DatabaseService().currentUser(box.read("currentUserId"));
+
+  setState(() {
+      _user= user;
+  });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +71,7 @@ class _NavScreenState extends State<NavScreen> {
               ? PreferredSize(
                   preferredSize: Size(screenSize.width, 100.0),
                   child: CustomAppBar(
-                    currentUser: currentUser,
+                    currentUser: _user,
                     icons: _icons,
                     selectedIndex: _selectedIndex,
                     onTap: (index) => setState(() => _selectedIndex = index),
