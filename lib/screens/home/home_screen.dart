c26c6710de8941dc2b23b35bc72a7f5570695a52
Mohammed Arbaoui <arb.med241@gmail.com>
2021-02-08 18:39:59 +0100

    edit home screen content

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:web/config/palette.dart';
 import 'package:web/data/data.dart';
+import 'package:web/models/UserModel.dart';
 import 'package:web/models/post_model.dart';
+import 'package:web/screens/login/loginForm.dart';
+import 'package:web/services/database_service.dart';
 import 'package:web/widgets/widgets.dart';
 
 import 'home_screen_desktop.dart';
@@ -18,10 +21,24 @@ class _HomeScreenState extends State<HomeScreen> {
   final TrackingScrollController _trackingScrollController =
       TrackingScrollController();
 
+  UserModel _user;
   @override
   void dispose() {
     _trackingScrollController.dispose();
     super.dispose();
+  }
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentUserInfo();
+  }
+
+  _getCurrentUserInfo()async{
+    UserModel user = await DatabaseService().currentUser(box.read("currentUserId"));
+
+    setState(() {
+      _user= user;
+    });
   }
 
   @override
@@ -31,7 +48,7 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Scaffold(
         body: Responsive(
           mobile:
-              HomeScreenMobile(scrollController: _trackingScrollController),
+              HomeScreenMobile(scrollController: _trackingScrollController, currentUser: _user,),
           desktop:
               HomeScreenDesktop(scrollController: _trackingScrollController),
         ),
--- lib/screens/home/home_screen_mobile.dart
+++ lib/screens/home/home_screen_mobile.dart
@@ -1,46 +1,64 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:web/config/palette.dart';
 import 'package:web/data/data.dart';
+import 'package:web/models/UserModel.dart';
 import 'package:web/models/models.dart';
 import 'package:web/screens/login/logscreen.dart';
+import 'package:web/screens/profil/profile_screen.dart';
 import 'package:web/screens/search/search_screen.dart';
+import 'package:web/widgets/custom_app_bar_mobile.dart';
 import 'package:web/widgets/widgets.dart';
 
-class HomeScreenMobile extends StatelessWidget {
+class HomeScreenMobile extends StatefulWidget {
   final TrackingScrollController scrollController;
+  final UserModel currentUser;
 
-  const HomeScreenMobile({Key key, @required this.scrollController})
+  const HomeScreenMobile(
+      {Key key, @required this.scrollController, @required this.currentUser})
       : super(key: key);
+
+  @override
+  _HomeScreenMobileState createState() => _HomeScreenMobileState();
+}
+
+class _HomeScreenMobileState extends State<HomeScreenMobile> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
-      controller: scrollController,
+      controller: widget.scrollController,
       slivers: [
         SliverAppBar(
           brightness: Brightness.light,
           backgroundColor: Colors.white,
-          title: Text(
-            "CatchMeOn",
-            style: const TextStyle(
-              color: Palette.catchMeOn_logo_Color,
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: -1.2,
-            ),
-          ),
+          title: SizedBox(
+              height: 80,
+              width: 100,
+              child: new Image.asset("assets/images/CMO_black.png")),
           centerTitle: false,
           floating: true,
           //avatar Image
           leading: new Padding(
             padding: const EdgeInsets.all(8.0),
             child: InkWell(
-              onTap: () => print("Avatar"),
-              child: CircleAvatar(
-                radius: 30,
-                backgroundImage: NetworkImage(currentUser.imageUrl),
-              ),
+              onTap: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => ProfileScreen(
+                            currentUserId: widget.currentUser.id,
+                            userId: widget.currentUser.id,
+                          )),
+                )
+              },
+              child: ProfileAvatar(
+                  imageUrl: ("assets/images/user_placeholder.jpg")),
             ),
           ),
 
@@ -54,19 +72,9 @@ class HomeScreenMobile extends StatelessWidget {
               ),
             ),
             CircleButton(
-              icon: MdiIcons.instagram,
-              iconSize: 30.0,
-              onPressed: () => print('instagram'),
-            ),
-            CircleButton(
-              icon: MdiIcons.twitter,
-              iconSize: 30.0,
-              onPressed: () => print('twitter'),
-            ),
-            CircleButton(
               icon: MdiIcons.logout,
               iconSize: 30.0,
-              onPressed: () =>  Navigator.push(
+              onPressed: () => Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => AuthThreePage()),
               ),
@@ -75,14 +83,39 @@ class HomeScreenMobile extends StatelessWidget {
         ),
         SliverList(
             delegate: SliverChildBuilderDelegate(
-                  (context, index) {
-                final Post post = posts[index];
-                return PostContainer(post: post);
-              },
-              childCount: posts.length,
-            )
-        ),
+          (context, index) {
+            return Column(
+              children: [
+                Container(
+                    margin: EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+                    child:
+                        Image.asset("assets/images/CMO_CatchMeOn_black.png")),
+                SizedBox(
+                  height: 50,
+                ),
+                Container(
+                    margin: EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+                    child: Expanded(
+                        child: Text(
+                      "CatchMeOn let you see the last content that your favourite creator put all over his social media in one place, no more wasting time switching between all the apps + you can't get lost in algorithm anymore!",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ))),
+                Row(
+                  children: [
+                    Expanded(
+                        child: Container(
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 100, vertical: 20),
+                            child: Image.asset("assets/gifs/cmo.gif"))),
+                  ],
+                ),
+              ],
+            );
+          },
+          childCount: 1,
+        )),
       ],
+      semanticChildCount: 1,
     );
   }
 }
